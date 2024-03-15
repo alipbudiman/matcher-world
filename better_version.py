b39c64b8333353ab655e1c856a7e11081ae64f10
Alif Budiman <82330418+alipbudiman@users.noreply.github.com>
2024-03-16 00:17:51 +0700

    Update better_version.py

--- better_version.py
+++ better_version.py
@@ -1,7 +1,8 @@
 #/// making text correction with python, using basic modul re and difflib.
 import difflib
 
-text = "helew werld"
-word = ["hello alip","hello world","hehe saya tamvan", "saya alif", "aku cinta python", "aku benci java script"]
+def matchword(text:str, *word:str) -> str:
+    ret = ["Did you means?"];data = list(filter(lambda w: int(str(str(difflib.SequenceMatcher(None, text.lower(), w.lower()).ratio()).split(".")[1])[0:1]) >= 4, word));[ret.append(f"{i+1}. {x}") for i,x in enumerate(data)];return ret
 
-print("Did you means?");data = list(filter(lambda w: int(str(str(difflib.SequenceMatcher(None, text.lower(), w.lower()).ratio()).split(".")[1])[0:1]) >= 4, word));[print(i + 1,". ",x) for i,x in enumerate(data)]
+data = matchword("helew werld", "hello alip","hello world","hehe saya tamvan", "saya alif", "aku cinta python", "aku benci java script")
+[print(x) for x in data]
